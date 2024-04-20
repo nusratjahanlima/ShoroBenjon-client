71540b56a504285c5a130f9e76789693c1386fb8
nusrat jahan <nusratjahanlima639@gmail.com>
2024-04-20 22:07:51 +0600

    done

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -16,6 +16,23 @@ const LoginSignup = () => {
 
     const login = async () =>{
         console.log("Login Function Executed",formData);
+        let responseData;
+        await fetch('http://localhost:4000/login',{
+            method:'POST',
+            headers:{
+                Accept:'application/json',
+                'Content-Type':'application/json',
+            },
+            body: JSON.stringify(formData),
+        }).then((response)=> response.json()).then((data)=>responseData=data)
+
+        if(responseData.success){
+            localStorage.setItem('auth-token',responseData.token);
+            window.location.replace("/");
+        }
+        else{
+            alert(responseData.errors)
+        }
     }
 
     const signup = async () =>{
@@ -28,10 +45,14 @@ const LoginSignup = () => {
                 'Content-Type':'application/json',
             },
             body: JSON.stringify(formData),
-        }).then((Response)=> Response.json()).then((data)=>responseData)
+        }).then((response)=> response.json()).then((data)=>responseData=data)
 
         if(responseData.success){
-            
+            localStorage.setItem('auth-token',responseData.token);
+            window.location.replace("/");
+        }
+        else{
+            alert(responseData.errors)
         }
     }
 
